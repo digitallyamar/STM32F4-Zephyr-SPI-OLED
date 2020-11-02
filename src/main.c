2bfdcc479741441da08037d59f9da91fa4ee9df6
digitallyamar <digitallyamar@gmail.com>
2020-11-02 17:12:50 +0530

    A dummy main.c template file. To be updated later.

---
+++ src/main.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020
+ * Author: Amar (@digitallyamar)
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/gpio.h>
+
+
+void main(void)
+{
+}
